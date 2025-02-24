ed262cd8048827f9f0754163dc067a941eceba3c
Janderson Jean <jandersonjean@gmail.com>
2025-02-24 14:25:12 -0300

    Adicionando Método

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,11 +27,14 @@ public class Program {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+//		double p = (x.a + x.b + x.c) / 2.0;
+//		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+//		
+//		p = (y.a + y.b + y.c) / 2.0;
+//		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 		
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("\nTriangle x area %.4f", areaX);
 		System.out.printf("\nTriangle y area %.4f", areaY);
